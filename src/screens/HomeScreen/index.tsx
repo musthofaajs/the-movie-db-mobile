c77ead1fd6634702671fba3d1b2c551cc0179a2d
musthofaajs <musthofaaaah@gmail.com>
2023-08-14 04:46:09 +0700

    Add Featured Movie

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,7 +1,16 @@
-import {StackNavigationProp} from '@react-navigation/stack';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  NavigationProp,
+  ParamListBase,
+  RouteProp,
+  useIsFocused,
+} from '@react-navigation/native';
 import React, {useEffect} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import FastImage from 'react-native-fast-image';
 import {TypedUseSelectorHook, useSelector} from 'react-redux';
+import {width} from '../../Theme/Layout';
+import {FocusAwareStatusBar} from '../../components/Atom';
 import {Container, Content} from '../../components/Organism';
 import {
   MOVIE_LIST_TYPE_NOW_PLAYING,
@@ -15,26 +24,39 @@ import {Movie} from '../../redux/types';
 import store, {RootState} from '../../store';
 import MovieSlider from './components/MovieSlider';
 
-type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+type HomeScreenNavigationProp = RouteProp<RootStackParamList, 'Home'>;
 
 type Props = {
-  navigation: HomeScreenNavigationProp;
+  navigation: NavigationProp<ParamListBase>;
+  route: HomeScreenNavigationProp;
 };
 
 const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 const HomeScreen: React.FC<Props> = ({navigation}) => {
+  const isFocused = useIsFocused();
   const {dispatch} = store;
   const {now_playing, popular, upcoming, top_rated} = useTypedSelector(
     state => state.movie.movieSlider,
   );
 
+  const [featuredMovie, setFeaturedMovie] = React.useState<Movie | null>(null);
+
   useEffect(() => {
-    dispatch(fetchMovies(MOVIE_LIST_TYPE_NOW_PLAYING) as any);
-    dispatch(fetchMovies(MOVIE_LIST_TYPE_POPULAR) as any);
-    dispatch(fetchMovies(MOVIE_LIST_TYPE_UPCOMING) as any);
-    dispatch(fetchMovies(MOVIE_LIST_TYPE_TOP_RATED) as any);
-  }, [dispatch]);
+    if (isFocused) {
+      dispatch(fetchMovies(MOVIE_LIST_TYPE_NOW_PLAYING) as any);
+      dispatch(fetchMovies(MOVIE_LIST_TYPE_POPULAR) as any);
+      dispatch(fetchMovies(MOVIE_LIST_TYPE_UPCOMING) as any);
+      dispatch(fetchMovies(MOVIE_LIST_TYPE_TOP_RATED) as any);
+    }
+  }, [isFocused]);
+
+  useEffect(() => {
+    if (now_playing && now_playing.length > 0) {
+      const randomIndex = Math.floor(Math.random() * now_playing.length);
+      setFeaturedMovie(now_playing[randomIndex]);
+    }
+  }, [now_playing]);
 
   const navigateToMovieDetail = (movie: Movie) => {
     navigation.navigate('MovieDetail', {movie});
@@ -42,7 +64,26 @@ const HomeScreen: React.FC<Props> = ({navigation}) => {
 
   return (
     <Container style={styles.container}>
+      <FocusAwareStatusBar
+        translucent={true}
+        backgroundColor={'transparent'}
+        barStyle={'light-content'}
+      />
       <Content>
+        {featuredMovie && (
+          <TouchableOpacity
+            style={styles.featuredContainer}
+            onPress={() => navigateToMovieDetail(featuredMovie)}>
+            <FastImage
+              source={{
+                uri: `https://image.tmdb.org/t/p/w1280${featuredMovie.backdrop_path}`,
+              }}
+              style={styles.featuredImage}
+              resizeMode={FastImage.resizeMode.cover}>
+              <Text style={styles.featuredTitle}>{featuredMovie.title}</Text>
+            </FastImage>
+          </TouchableOpacity>
+        )}
         <MovieSlider
           title="Now Playing"
           movies={now_playing}
@@ -72,6 +113,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
+  },
+  featuredContainer: {
+    overflow: 'hidden',
+    paddingVertical: width * 0.0795,
+    paddingHorizontal: width * 0.025,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  featuredImage: {
+    flex: 1,
+    borderWidth: 0.1,
+    borderColor: '#7e7e7e',
+    width: width * (1 - 0.025),
+    height: undefined,
+    aspectRatio: 9 / 16,
+    justifyContent: 'flex-end',
+    padding: 16,
+    borderRadius: width * 0.015,
+    shadowColor: '#7e7e7e',
+    shadowOffset: {
+      width: 0,
+      height: 11,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 11.78,
+    elevation: 15,
+  },
+  featuredTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 8,
   },
 });
 
